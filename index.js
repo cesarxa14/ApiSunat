687d0005ab72fb875302863dd33919939abad947
Cesar Torres <cesarf9t@hotmail.com>
2023-08-06 15:56:02 -0500

    mejoras

--- index.js
+++ index.js
@@ -9,25 +9,29 @@ app.listen(app.get('port'), ()=>{
     console.log('Server on port', app.get('port'));
 })
 
+// RUTA PARA OBTENER DATOS DE LA SUNAT
 app.get('/', async (req, res) => {
-    let data = null;
-    let config = {
-        method: 'get',
-        url: 'https://api.apis.net.pe/v1/tipo-cambio-sunat',
-        headers: {
-            // 'Authorization': 'Bearer bsOzCDXPQZxOi5rhGbMuraa1vaL2w2lPjaFm6iYcM5DFle3hmRmbfZSZGdzA',
-            'Accept': 'application/json'
-            // ...data.getHeaders()
-        }
-    };
+    try {
+        let data = null;
+        let config = {
+            method: 'get',
+            url: 'https://api.apis.net.pe/v1/tipo-cambio-sunat',
+            headers: {
+                'Accept': 'application/json'
+            }
+        };
 
-    axios(config).then((response) => {
-        console.log('response', response)
-        data = response.data;
-        return res.send(data)
-    }).catch ((error) => {
-        console.log(error)
-    })
+        axios(config).then((response) => {
+            console.log('response', response)
+            data = response.data;
+            return res.send(data)
+        }).catch ((error) => {
+            console.log(error)
+        })
+    } catch(err) {
+        console.log(err);
+    }
+    
 
     
 })
